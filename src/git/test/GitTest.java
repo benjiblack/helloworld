9e0ceb6d2776d44a6590a439906112e7e406fd49
Ben Black <ben@cds.digital>
2017-01-19 20:15:00 -0600

    Changed local variable to global and deleted String in main()

--- src/git/test/GitTest.java
+++ src/git/test/GitTest.java
@@ -4,11 +4,10 @@ import javax.swing.JOptionPane;
 
 public class GitTest {
     
+    public static String message = "Global variable";
+    
     public static void main(String[] args) {
-        String message = "This is a test";
         String title = "Git Test 1.0";
         JOptionPane.showConfirmDialog(null, message, title, JOptionPane.PLAIN_MESSAGE);
-        String microsoft = "Microsoft Windows";
     }
-    
 }
